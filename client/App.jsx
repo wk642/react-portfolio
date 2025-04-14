6842056a6a3fc5b798f518ecc911e7a352e1c26e
Winnie Kelley <w642kelley@gmail.com>
2025-04-13 23:05:13 -0500

    Cleaned up initial files, added tailwind

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1 className="text-orange-500 text-center text-6xl p-10">Winnie Kelley</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
